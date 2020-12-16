2b95df6a86909cc7cb4d2b56d4bc80f552e3a687
xFA25E <55886566+xFA25E@users.noreply.github.com>
2020-12-16 12:42:32 +0100

    refactored a lot

--- shell-pwd.el
+++ shell-pwd.el
@@ -39,23 +39,6 @@
 (require 'files)
 (require 'tramp)
 
-(defgroup shell-pwd nil
-  "Display shell working directory in buffer name."
-  :group 'extensions)
-
-(defcustom shell-pwd-generate-buffer-name-function
-  #'shell-pwd-generate-buffer-name
-  "Function called when generating new buffer name.
-Should accept one string argument and return a string.  Returned
-string will be used as buffer name."
-  :type 'function
-  :group 'shell-pwd)
-
-(defcustom shell-pwd-shorten-directory t
-  "Whether default name generating function should shorten directory or not."
-  :type 'boolean
-  :group 'shell-pwd)
-
 (defvar shell-pwd--previous-directory ""
   "Variable used to check for directory change.")
 (make-variable-buffer-local 'shell-pwd--previous-directory)
@@ -73,20 +56,24 @@ string will be used as buffer name."
 
 If `DIRECTORY' does not end with a slash, it will be considered
 as a file.
-/home/user/some/file -> /h/u/some/file"
-  (save-match-data
-    (if (string-match (rx bos
-                          (group (+? anything) "/")
-                          (group (+ (not "/")) "/" (* (not "/")))
-                          eos)
-                      directory)
-        (let ((to-shorten (match-string 1 directory))
-              (unchanged (match-string 2 directory)))
-          (thread-first (mapcar #'shell-pwd--cut-file-name
-                                (split-string to-shorten "/"))
-            (string-join "/")
-            (concat unchanged)))
-      directory)))
+/home/user/some/file -> /h/u/some/file
+
+It is abbreviated with `ABBREVIATE-FILE-NAME' first, so user
+directory may change to tilde."
+  (let ((directory (abbreviate-file-name directory)))
+    (save-match-data
+      (if (string-match (rx bos
+                            (group (+? anything) "/")
+                            (group (+ (not "/")) "/" (* (not "/")))
+                            eos)
+                        directory)
+          (let ((to-shorten (match-string 1 directory))
+                (unchanged  (match-string 2 directory)))
+            (thread-first (mapcar #'shell-pwd--cut-file-name
+                                  (split-string to-shorten "/"))
+              (string-join "/")
+              (concat unchanged)))
+        directory))))
 
 (defun shell-pwd-generate-buffer-name (dir)
   "Generate new shell buffer name based on `DIR'.
@@ -94,44 +81,21 @@ If `DIR' is a remote directory, add tramp host and method to generated name"
   (let ((name (or (file-remote-p dir 'localname) dir))
         (host (if-let ((host (file-remote-p dir 'host))) (concat host " ") ""))
         (user (if-let ((user (file-remote-p dir 'user))) (concat user "@") "")))
-    (format "*sh %s%s%s*" user host
-            (if shell-pwd-shorten-directory
-                (shell-pwd-shorten-directory (abbreviate-file-name name))
-              name))))
+    (format "*sh %s%s%s*" user host (shell-pwd-shorten-directory name))))
 
 ;;;###autoload
 (defun shell-pwd-directory-tracker (&rest _args)
   "The main function that tracks directory change.
 Put this in `comint-input-filter-functions' after
 `shell-directory-tracker'."
-  (if (not (string-equal default-directory
-                         shell-pwd--previous-directory))
-      (let ((name (funcall shell-pwd-generate-buffer-name-function
-                           default-directory)))
-        (rename-buffer name t))
+  (unless (string= default-directory shell-pwd--previous-directory)
+    (rename-buffer (shell-pwd-generate-buffer-name default-directory) t)
     (setq shell-pwd--previous-directory default-directory)))
 
 ;;;###autoload
 (defun shell-pwd-enable ()
   "Put this into the `shell-mode-hook'."
-  (add-hook 'comint-input-filter-functions
-            #'shell-pwd-directory-tracker
-            t t))
-
-;;;###autoload
-(cl-defun shell-pwd-switch-to-buffer (&optional (directory default-directory))
-  "Show a list of shell buffers and suggest to create a new one in `DIRECTORY'."
-  (interactive
-   (list (if current-prefix-arg
-             (expand-file-name (read-directory-name "Default directory: "))
-           default-directory)))
-  (cl-flet ((shell-buffer-p (b) (eq (buffer-local-value 'major-mode b) 'shell-mode))
-            (pwd-buffer () (shell-pwd-generate-buffer-name directory)))
-    (let* ((buffer-name (generate-new-buffer-name (pwd-buffer)))
-           (shell-buffers (mapcar #'buffer-name (cl-delete-if-not #'shell-buffer-p (buffer-list))))
-           (name (completing-read "Shell buffer: " (cons buffer-name shell-buffers))))
-      (if-let ((buffer (get-buffer name))) (pop-to-buffer buffer) (shell name))))
-  (shell-pwd-enable))
+  (add-hook 'comint-input-filter-functions #'shell-pwd-directory-tracker t t))
 
 (provide 'shell-pwd)
 
